5f2270e57383a4a9054857f31170b70f2dfb6670
jimmy914 <105338878+jimmy914@users.noreply.github.com>
2025-11-04 17:01:13 +0900

    [feat] : texteditingcontroller 작업 진행

---
+++ lib/pages/input_information_page/change_information_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../components/appbar/app_bar_with_title_without_hamburger.dart';
+import '../../components/button/bottom_navigation_bar_button.dart';
+import '../personal_page/personal_page.dart';
+import 'components/check_list.dart';
+
+//TODO: Exception Handling for TextField
+class ChangeInformationPage extends StatelessWidget {
+  const ChangeInformationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBarWithTitleWithoutHamburger(title: '개인 정보 입력'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Column(
+          // TODO: Connect TextEditingController for each CheckList
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                '인바디 (InBody) 결과를 필요로 합니다!',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                ),
+              ),
+            ),
+            CheckList(checkName: '체중 (kg) : ', checkController: TextEditingController()),
+            CheckList(checkName: '골격근량 : ', checkController: TextEditingController()),
+            CheckList(checkName: '체지방률 : ', checkController: TextEditingController()),
+            CheckList(checkName: '체지방 지수 (BMI) : ', checkController: TextEditingController()),
+            CheckList(checkName: '기초대사량 (BMR) : ', checkController: TextEditingController()),
+            CheckList(checkName: '평균 운동 시간 (분) : ', checkController: TextEditingController()),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBarButton(
+          buttonName: '수정 완료하기',
+          onPressed: () {
+            Get.offAll(() => PersonalPage());
+          }),
+    );
+  }
+}
