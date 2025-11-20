5647172acdb59271d89bd200402f4ae4c21c4a85
47813 <vanguard0956@gmail.com>
2025-11-20 16:18:06 +0900

    [FEAT]: 커뮤니티 페이지 데이터 연동

--- lib/controllers/community_controller.dart
+++ lib/controllers/community_controller.dart
@@ -1,16 +1,83 @@
 import 'package:get/get.dart';
 
-import '../services/community/get_similar.dart';
+import '../services/http/community/get_recent.dart';
+import '../services/http/community/get_similar.dart';
 
 class CommunityController extends GetxController {
-  RxString nickname = ''.obs;
-  RxString category = ''.obs;
-  RxString kcal = ''.obs;
-  RxString satisfaction = ''.obs;
+  RxList<Map<String, dynamic>> totalRecommendedWorkout = <Map<String, dynamic>>[].obs;
+  RxList<Map<String, dynamic>> totalRecentWorkout = <Map<String, dynamic>>[].obs;
+  RxBool isLoading = false.obs;
+
+  Future<void> fetchData() async {
+    isLoading.value = true;
+    try {
+      await fetchSimilar();
+      await fetchRecent();
+    } catch (e) {
+      print("CommunityControllerError: $e");
+    } finally {
+      isLoading.value = false;
+    }
+  }
 
   Future<void> fetchSimilar() async {
     final rawData = await getSimilar();
-    nickname.value = rawData[1]['user']['nickname'];
-    print(nickname.value);
+    print(rawData);
+    final recommendedWorkoutCount = rawData['workouts'].length;
+
+    for (int i = 0; i < recommendedWorkoutCount; i++) {
+      final nickname = rawData['workouts'][i]['user']['nickname'];
+      final id = rawData['workouts'][i]['log_id'];
+      final category = rawData['workouts'][i]['routine']['category'];
+      final kcal = rawData['workouts'][i]['kcal'];
+      final satisfaction = rawData['workouts'][i]['satisfaction'];
+      final height = rawData['workouts'][i]['user']['height'];
+      final weight = rawData['workouts'][i]['user']['weight'];
+      final totalTime = rawData['workouts'][i]['total_time'];
+      final formData = {
+        "nickname": nickname,
+        "id": id,
+        "category": category,
+        "kcal": kcal,
+        "satisfaction": satisfaction,
+        "height": height,
+        "weight": weight,
+        "totalTime": totalTime,
+      };
+      totalRecommendedWorkout.add(formData);
+    }
+  }
+
+  Future<void> fetchRecent() async {
+    final rawData = await getRecent();
+    print(rawData);
+    final recentWorkoutCount = rawData['workouts'].length;
+
+    for (int i = 0; i < recentWorkoutCount; i++) {
+      final nickname = rawData['workouts'][i]['user']['nickname'];
+      final id = rawData['workouts'][i]['log_id'];
+      final category = rawData['workouts'][i]['routine']['category'];
+      final kcal = rawData['workouts'][i]['kcal'];
+      final satisfaction = rawData['workouts'][i]['satisfaction'];
+      final height = rawData['workouts'][i]['user']['height'];
+      final weight = rawData['workouts'][i]['user']['weight'];
+      final totalTime = rawData['workouts'][i]['total_time'];
+      final formData = {
+        "nickname": nickname,
+        "id": id,
+        "category": category,
+        "kcal": kcal,
+        "satisfaction": satisfaction,
+        "height": height,
+        "weight": weight,
+        "totalTime": totalTime,
+      };
+      totalRecentWorkout.add(formData);
+    }
+  }
+
+  Future<void> disposeController() async {
+    totalRecommendedWorkout.value = <Map<String, dynamic>>[].obs;
+    totalRecentWorkout.value = <Map<String, dynamic>>[].obs;
   }
 }
